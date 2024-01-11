6bc8b2ce3b4bf755819e3c441485c90c43661d29
asuzukosi <keloasuzu@yahoo.com>
2024-01-11 03:19:02 +0000

    feat: start implementation of decision transformers

---
+++ src/DecisionTransformers/DecisionTranformers.py
@@ -0,0 +1,17 @@
+
+
+from typing import Any
+
+
+class ActionTokens():
+    pass
+
+class StateTokens():
+    pass
+
+class DecisionTransformer():
+    def __init__(self) -> None:
+        pass
+    
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        pass
